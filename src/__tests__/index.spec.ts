b2e16c1f52d989cee17bae6442b35b0568e02ff0
Ryan Simpson <ryan.simpson@test-and-trace.nhs.uk>
2022-03-22 10:57:06 +0000

    binary chop

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,7 +1,16 @@
-import { sum } from '..';
+import { chop } from '..';
 
-describe('sum', () => {
-  it('1 + 1 = 2', () => {
-    expect(sum(1, 1)).toBe(2);
+describe('chop', () => {
+  it.each`
+    target | array  | result
+    ${1}   | ${[1]} | ${0}
+    ${1}   | ${[1, 2, 3, 4, 5]} | ${0}
+    ${2}   | ${[1, 2, 3, 4, 5]} | ${1}
+    ${3}   | ${[1, 2, 3, 4, 5]} | ${2}
+    ${4}   | ${[1, 2, 3, 4, 5]} | ${3}
+    ${5}   | ${[1, 2, 3, 4, 5]} | ${4}
+    ${170}   | ${[10, 20, 80, 30, 60, 50, 110, 100, 130]} | ${-1}
+  `('finds $target in $array at index $result', ({ target, array, result }) => {
+    expect(chop(target, array)).toBe(result);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-export const sum = (a: number, b: number) => a + b;
+export const chop = (target: number, array: number[], start = 0, end = array.length): number => {
+  const currentArray = array.slice(start, end);
+  const halfWayIndex = Math.floor(currentArray.length / 2);
+
+  if (array[halfWayIndex] === target) {
+    return halfWayIndex;
+  } else if (array[halfWayIndex] < target) {
+    return chop(target, array.slice(halfWayIndex)) + halfWayIndex;
+  } else {
+    return chop(target, array.slice(0, halfWayIndex));
+  }
+};
