e1ff300b331b90d5f8e20b7753abba68480c4c6e
Ryan Simpson <ryan.simpson@test-and-trace.nhs.uk>
2022-06-21 11:40:42 +0100

    parsing one and two using multi line map

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,6 +1,12 @@
-import { parseNumber } from '..';
+import { parseNumber,  splitStringToThrees } from '..';
 
 // expect(parseNumber(1, 1)).toBe(2);
+
+describe('splitStringToThrees', () => {
+  it('splits the string into threes', () => {
+    expect(splitStringToThrees(`    _ `)).toEqual(['   ', ' _ '])
+  })
+})
 
 describe('parseNumber', () => {
   it('parses the number 1', () => {
@@ -9,4 +15,11 @@ describe('parseNumber', () => {
   |`
     )).toBe(1);
   });
+  it('parses the number 12', () => {
+    expect (parseNumber(
+`    _ 
+  | _|
+  ||_ `
+     )).toBe(12);
+   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,50 @@
 const maps = {
     [`   
   |
-  |`]: 1
+  |`]: 1,
+  [` _ 
+ _|
+|_ `]: 2
 };
 
-export const parseNumber = (digit: string) => {
-    console.log(digit)
-    return maps[digit]
+export const splitStringToThrees = (str: string) => {
+  const x: string[] = [];
+
+  for(let i = 0; i < str.length; i+=3) {
+    x.push(str.slice(i,i+3));
+  }
+  return x;
 }
+
+export const parseNumber = (file: string) => {
+  //[
+  //"    _ "
+  //"  | _|"
+  //"  ||_ "
+  //]
+
+  //[
+  //["   ", " _ "]
+  //["  |", " _|"]
+  //["  |", "|_ "]
+  //]
+  
+  //one = ["   ", "  |", "  |"]
+  //two - [" _ ", " _|", "|_ "]
+
+  //  [  _ 
+  //  | _|
+  //  ||_ ]
+
+    const lines: string[][] = file.split('\n').map(splitStringToThrees);
+
+    const one = [lines[0][0], lines[1][0], lines[2][0]].join('\n');
+    const two = [lines[0][1], lines[1][1], lines[2][1]].join('\n');
+
+    return parseInt(`${maps[one]}${maps[two]}`)
+}
+
+// export const parseLine = (file: string) => {}
 
 
 // const fileContents = 
@@ -20,21 +57,21 @@ export const parseNumber = (digit: string) => {
 //   ||_  _|  | _||_|  ||_| _|
 // `;
 
-1 = [
-  ['  ']
-  [' |']
-  [' |']
-]
+// 1 = [
+//   ['  ']
+//   [' |']
+//   [' |']
+// ]
 // .join('\n')
 
-const map = {
-  '   ': {
-    '  |': {
-      '  |': 1
-    },
-    '|_|': 4, 
-  },
-  ' _ ': {
+// const map = {
+//   '   ': {
+//     '  |': {
+//       '  |': 1
+//     },
+//     '|_|': 4, 
+//   },
+//   ' _ ': {
 
-  }
-}
+//   }
+// }
